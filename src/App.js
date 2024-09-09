727f7bc9d90eea2dfbfa7ffa123132f8301698e3
atulks26 <atulksingh04@gmail.com>
2024-09-10 00:39:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import WhatWeOffer from './pages/Offers';
-import TrainingWorkshops from './pages/Workshop';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Landing from "./pages/Landing";
+import WhatWeOffer from "./pages/Offers";
+import TrainingWorkshops from "./pages/Workshop";
 // import DiscussionForum from './pages/DiscussionForum';
-import Projects from './pages/ProjectSlider';
+import Projects from "./pages/ProjectSlider";
 
 function App() {
-  return (
-    <div>
-      <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/what-we-offer" element={<WhatWeOffer />} />
-        <Route path="/training-workshops" element={<TrainingWorkshops />} />
-        {/* <Route path="/discussion-forum" element={<DiscussionForum />} /> */}
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-      <Footer />
-    </Router>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/what-we-offer" element={<WhatWeOffer />} />
+                    {/* <Route path="/training-workshops" element={<TrainingWorkshops />} />
+                    <Route path="/discussion-forum" element={<DiscussionForum />} />
+                    <Route path="/projects" element={<Projects />} /> */}
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
