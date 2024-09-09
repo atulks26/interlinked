5bc5ed26e607cc63c9801398ae5a21fea2f996bd
Sanchita <sakshibhardwaj14@gmail.com>
2024-09-09 22:09:55 +0530

    message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,80 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg">Interdepartmental Platform</div>
-        <ul className="flex space-x-4 text-white">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/what-we-offer">What We Offer</Link></li>
-          <li><Link to="/training-workshops">Training & Workshops</Link></li>
-          <li><Link to="/discussion-forum">Discussion Forum</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-        </ul>
-        <div className="flex space-x-4">
-          <button className="text-white bg-blue-600 px-4 py-2 rounded">Login</button>
-          <button className="text-white bg-green-600 px-4 py-2 rounded">Register</button>
-        </div>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="path_to_mcd_logo.png" alt="MCD Logo" />
+        <h1>Municipal Corporation of Delhi</h1>
       </div>
+
+      <div className="navbar-links">
+        <a href="#home">Home Page</a>
+        <a href="#about">About Us</a>
+
+        {/* Dropdown for Departments */}
+        <div className="dropdown">
+          <a href="#departments" className="dropdown-toggle">Departments</a>
+          <div className="dropdown-content">
+            <div className="dropdown-section">
+              <h4>Column 1</h4>
+              <a href="#">ADVERTISEMENT</a>
+              <a href="#">ARCHITECTURE DEPARTMENT</a>
+              <a href="#">ASSESSMENT AND COLLECTION DEPARTMENT</a>
+              <a href="#">AYUSH DEPARTMENT</a>
+              <a href="#">BUILDING DEPARTMENT</a>
+              <a href="#">CENTRAL ESTABLISHMENT</a>
+              <a href="#">COMMITTEE AND CORPORATION</a> 
+              <a href="#">MUNICIPAL SECRETARY OFFICE</a>
+              <a href="#">Organization and Method department</a>
+              <a href="#">COMMUNITY SERVICES</a>
+            </div>
+            <div className="dropdown-section">
+              <h4>Column 2</h4>
+              <a href="#">DIRECTORATE OF INQUIRY</a>
+              <a href="#">DIRECTORATE OF PRESS AND INFORMATION</a>
+              <a href="#">Department of Environmental Management</a>
+              <a href="#">EDUCATION</a>
+              <a href="#">ELECTION DEPARTMENT</a>
+              <a href="#">ENGINEERING DEPARTMENT</a>
+              <a href="#">Electrical And Mechanical Department</a>
+              <a href="#">PUBLIC HEALTH DEPARTMENT</a>
+              <a href="#">REMUNERATIVE PROJECT CELL</a>
+              <a href="#">STATUTORY AUDIT DEPARTMENT</a>
+              <a href="#">FACTORY LICENSE</a>
+              <a href="#">FINANCE DEPARTMENT</a>
+              
+            </div>
+            <div className="dropdown-section">
+              <h4>Column 3</h4>
+              <a href="#">HACKNEY CARRIAGE</a>
+              <a href="#">HORTICULTURE DEPARTMENT</a>
+              <a href="#">HOSPITAL ADMINISTRATION</a>
+              <a href="#">INFORMATION TECHNOLOGY</a>
+              <a href="#">LABOUR WELFARE DEPARTMENT</a>
+              <a href="#">LAND AND ESTATE</a>
+              <a href="#">LANGUAGE DEPARTMENT</a>
+              <a href="#">LAW DEPARTMENT</a>
+              <a href="#">LICENSING DEPARTMENT</a>
+              <a href="#">TOWN PLANNING</a>
+              <a href="#">Toll Tax</a>
+              <a href="#">VETERINARY</a>
+              <a href="#">VIGILANCE</a>
+            </div>
+          </div>
+        </div>
+
+        <a href="#zones">Zones</a>
+        <a href="#downloads">Downloads</a>
+        <a href="#tenders">Tenders</a>
+      </div>
+
+      <div className="navbar-flag">
+        <img src="path_to_india_flag.png" alt="Indian Flag" />
+      </div>
+
+      <a href="#online-services" className="navbar-online-services">Online Services</a>
     </nav>
   );
 }
